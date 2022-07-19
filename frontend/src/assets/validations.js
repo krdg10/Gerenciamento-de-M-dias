04598ba56f729136455a681f8973860bfc8c4e88
krdg10 <krdg10@live.com>
2022-07-19 04:51:58 -0300

    validação de campos do inserir imovel

---
+++ frontend/src/assets/validations.js
@@ -0,0 +1,40 @@
+import { defineRule, configure } from 'vee-validate';
+//import { required, email, numeric, digits, max, min, min_value, max_value, integer, regex, required_if } from '@vee-validate/rules';
+import { required, max, numeric, min_value, digits } from '@vee-validate/rules';
+import pt_BR from '@vee-validate/i18n/dist/locale/pt_BR.json';
+import { localize } from '@vee-validate/i18n';
+
+
+defineRule('required', required);
+defineRule('max', max);
+defineRule('numeric', numeric);
+defineRule('min_value', min_value);
+defineRule('digits', digits);
+
+
+
+
+configure({
+  generateMessage: localize({
+    pt_BR,
+  }),
+});
+
+
+/*configure({
+  generateMessage: localize('pt_BR', {
+    messages: {
+      required: 'Esse campo é obrigatório.',
+      email: 'Digite email válido.',
+      numeric: 'Digite apenas números.',
+      min: 'Digite 0:{length} caracteres.',
+      max: 'Digite no máximo 0:{length} caracteres.',
+      digits: 'O {field} precisa de 0:{length} caracteres.', // length pra funcionar a variavel tem que ser desse jeito, field tbm. o max e min n sei como seria
+      min_value: 'O valor mínimo é {min}.',
+      integer: 'O valor não pode ser fracionário.',
+      max_value: 'O valor máximo é {max}.',
+      regex: 'Valor inválido.',
+      required_if: 'Esse campo é obrigatório.'
+    }
+  }),
+});*/
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -6,6 +6,8 @@ import axios from 'axios'
 import VueAxios from 'vue-axios'
 import { store } from './store/store'
 import router from './routes'
+import { setLocale } from '@vee-validate/i18n';
+setLocale('pt_BR');
 
 const app = createApp(App)
 app.use(VueAxios, axios)

757fca446310ff079d861f0c3eaff8dcc0ad40a6
krdg10 <krdg10@live.com>
2022-09-25 03:21:33 -0300

    Página não encontrada

--- frontend/src/components/Utils/DeuRuim.vue
+++ frontend/src/components/Utils/DeuRuim.vue
@@ -1,13 +1,5 @@
 <template>
-    <div class="container-fluid">
-        <p>Deu ruim :/</p>
+    <div class="container-fluid d-flex justify-content-center my-5">
+        <h1>Página não encontrada.</h1>
     </div>
-
 </template>
-
-<script>
-export default {
-
-}
-</script>
-

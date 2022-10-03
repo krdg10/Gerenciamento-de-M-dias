0667ae05a1acaee541af4b5581530cacd3362249
krdg10 <krdg10@live.com>
2022-10-03 03:29:50 -0300

    botoes pra trocar de pagina aparecendo so quando devem

--- frontend/src/components/Utils/PaginationOfLists.vue
+++ frontend/src/components/Utils/PaginationOfLists.vue
@@ -1,15 +1,22 @@
 <template>
     <div class="pagination">
-        <button v-for="(page, index) in pages" :key="page" class="item" :class="{ current: page === current }"
+        <button v-for="(page, index) in pages" :key="page" class="item"
+            v-bind:class="{ 'current': page === current, 'd-none': page != current && page != current-1 && page != current + 1 && page != last && page != 1  }"
             @click="changePage(index)">
-            {{ page }}
+            <a v-if="page==current+1"> --|</a>
+            <a v-if="page==current-1"> |--</a>
+            <a v-if="page!=current-1 && page!= current+1"> {{ page }}</a>
         </button>
     </div>
 </template>
   
 <script>
+// fazendo sumir com css... mas o ideal é: não entrar no v-for e/ou no array ter só os numeros corretos.
 export default {
     name: 'PaginationOfLists',
+    data() {
+        return { last: 1 }
+    },
     props: {
         offset: {
             type: [String, Number],
@@ -23,23 +30,31 @@ export default {
             type: [String, Number],
             default: 10,
         },
-    },
-    computed: {
-        current() {
-            return this.offset ? this.offset + 1 : 1;
+        current: {
+            type: [String, Number],
+            required: true,
         },
+    },
+
+    computed: {
         pages() {
             const qty = Math.ceil(this.total / this.limit);
-
-            if (qty <= 1) return [1];
-
-            return Array.from(Array(qty).keys(), (i) => i + 1);
+            if (qty <= 1) {
+                this.setLast(1);
+                return [1];
+            }
+            let response = Array.from(Array(qty).keys(), (i) => i + 1);
+            this.setLast(response[response.length - 1]);
+            return response;
         },
     },
     methods: {
         changePage(offset) {
             this.$emit('change-page', offset);
         },
+        setLast(number) {
+            this.last = number;
+        }
     },
 };
 </script>
